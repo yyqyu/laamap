d30cca1ba5b19b3c098f5452825e0a18789cf775
Viktor Jóba <jobes666@gmail.com>
2022-10-09 15:29:47 +0200

    feat(install dependecies): install transloco and angular material
    install transloco for localization\ninstall angular materialfor UI components

--- apps/mapview/src/app/app.module.ts
+++ apps/mapview/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgxMapLibreGLModule } from '@maplibre/ngx-maplibre-gl';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { TranslocoRootModule } from './transloco-root.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NgxMapLibreGLModule, HttpClientModule],
+  imports: [BrowserModule, NgxMapLibreGLModule, HttpClientModule, TranslocoRootModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ transloco.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  rootTranslationsPath: 'apps/mapview/src/assets/i18n/',
+  langs: ['en', 'sk'],
+  keysManager: {}
+};
