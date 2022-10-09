517d1184fdfca3fac943ac39c3183402dfe476fd
Viktor Jóba <jobes666@gmail.com>
2022-10-09 15:08:40 +0200

    feat(install dependency): install libre map
    install angular libre map library

--- apps/mapview/src/app/app.module.ts
+++ apps/mapview/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxMapLibreGLModule } from '@maplibre/ngx-maplibre-gl';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgxMapLibreGLModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
