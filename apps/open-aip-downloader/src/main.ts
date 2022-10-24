29020298a461bbfb68a1209a62e1d1444702e06e
Viktor Jóba <jobes666@gmail.com>
2022-10-24 22:06:28 +0200

    feat(openaip downloader): :sparkles: implement openaip downloader

--- apps/open-aip-downloader/src/main.ts
+++ apps/open-aip-downloader/src/main.ts
@@ -1,1 +1,33 @@
-console.log('Hello World!');
+import { Storage } from '@google-cloud/storage';
+import fs = require('fs');
+
+const bucketName = '29f98e10-a489-4c82-ae5e-489dbcd4912f';
+const destFilePath = `${__dirname}/open-aip-db`;
+
+if (!fs.existsSync(destFilePath)) {
+  fs.mkdirSync(destFilePath);
+}
+
+// Creates a client
+const storage = new Storage();
+
+async function downloadPublicFile() {
+  const files = await storage.bucket(bucketName).getFiles();
+  const interestingFiles = files[0]
+    .map((file) => file.name)
+    .filter((name) => name.startsWith('sk_') && name.endsWith('.geojson'));
+  console.log(interestingFiles);
+
+  for (const fileName of interestingFiles) {
+    const options = {
+      destination: `${destFilePath}/${fileName}`,
+    };
+    await storage.bucket(bucketName).file(fileName).download(options);
+    console.log(
+      `Downloaded public file ${bucketName} to ${destFilePath}/${fileName}`
+    );
+  }
+  console.log('Download complete');
+}
+
+downloadPublicFile().catch(console.error);
