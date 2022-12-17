020f4a380760c97ec715ac10ea4fe9db768f4d81
Viktor Jóba <jobes666@gmail.com>
2022-12-17 19:54:14 +0100

    feat(map): :sparkles: add own position cursor

--- apps/mapview/src/app/components/map/map.component.ts
+++ apps/mapview/src/app/components/map/map.component.ts
@@ -1,6 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { EventData } from '@maplibre/ngx-maplibre-gl';
+import { MapLibreEvent } from 'maplibre-gl';
 import { SettingsDialogComponent } from '../settings-dialog/settings-dialog.component';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+declare const AbsoluteOrientationSensor: any;
 
 @Component({
   selector: 'laamap-map',
@@ -9,9 +14,38 @@ import { SettingsDialogComponent } from '../settings-dialog/settings-dialog.comp
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MapComponent {
-  constructor(private dialog: MatDialog) {}
+  constructor(private dialog: MatDialog) {
+    if (`AbsoluteOrientationSensor` in window) {
+      const sensor = new AbsoluteOrientationSensor();
+      sensor.addEventListener(
+        'reading',
+        (e: { target: { quaternion: number[] } }) => {
+          const q = e.target.quaternion;
+          const heading =
+            Math.atan2(
+              2 * q[0] * q[1] + 2 * q[2] * q[3],
+              1 - 2 * q[1] * q[1] - 2 * q[2] * q[2]
+            ) *
+            (180 / Math.PI);
+          console.log('heading', heading);
+
+          document.documentElement.style.setProperty('--heading', `${heading}`);
+        }
+      );
+      sensor.start();
+    }
+  }
 
   openSettingsDialog(): void {
     this.dialog.open(SettingsDialogComponent, { width: '100%' }).afterClosed();
+  }
+
+  rotate(
+    event: MapLibreEvent<MouseEvent | TouchEvent | undefined> & EventData
+  ): void {
+    document.documentElement.style.setProperty(
+      '--bearing',
+      `${event.target.getBearing()}`
+    );
   }
 }
