c86feb1e98f25a87b3e1c2823f88d20cad136015
Viktor Jóba <jobes666@gmail.com>
2022-12-17 23:05:01 +0100

    feat(map): :bug: correct compass rientation

--- apps/mapview/src/app/components/map/map.component.ts
+++ apps/mapview/src/app/components/map/map.component.ts
@@ -15,25 +15,11 @@ declare const AbsoluteOrientationSensor: any;
 })
 export class MapComponent {
   constructor(private dialog: MatDialog) {
-    if (`AbsoluteOrientationSensor` in window) {
-      const sensor = new AbsoluteOrientationSensor();
-      sensor.addEventListener(
-        'reading',
-        (e: { target: { quaternion: number[] } }) => {
-          const q = e.target.quaternion;
-          const heading =
-            Math.atan2(
-              2 * q[0] * q[1] + 2 * q[2] * q[3],
-              1 - 2 * q[1] * q[1] - 2 * q[2] * q[2]
-            ) *
-            (180 / Math.PI);
-          console.log('heading', heading);
-
-          document.documentElement.style.setProperty('--heading', `${heading}`);
-        }
-      );
-      sensor.start();
-    }
+    this.requestCompassPermission().then((res) => {
+      if (res) {
+        this.setupCompass()?.start();
+      }
+    });
   }
 
   openSettingsDialog(): void {
@@ -47,5 +33,61 @@ export class MapComponent {
       '--bearing',
       `${event.target.getBearing()}`
     );
+  }
+
+  private async requestCompassPermission(): Promise<boolean> {
+    navigator.permissions.query({ name: 'screen-wake-lock' });
+    const results = await Promise.all([
+      navigator.permissions.query({ name: 'accelerometer' as never }),
+      navigator.permissions.query({ name: 'magnetometer' as never }),
+      navigator.permissions.query({ name: 'gyroscope' as never }),
+    ]);
+    return results.every((result) => result.state === 'granted');
+  }
+
+  private setupCompass(): { start: () => void } | undefined {
+    if (`AbsoluteOrientationSensor` in window) {
+      const sensor = new AbsoluteOrientationSensor();
+      sensor.addEventListener(
+        'reading',
+        (e: { target: { quaternion: number[] } }) => {
+          const q = e.target.quaternion;
+          const heading =
+            Math.atan2(
+              2 * q[0] * q[1] + 2 * q[2] * q[3],
+              1 - 2 * q[1] * q[1] - 2 * q[2] * q[2]
+            ) *
+            (-180 / Math.PI);
+
+          switch (screen.orientation.type) {
+            case 'landscape-primary':
+              document.documentElement.style.setProperty(
+                '--heading',
+                `${heading}`
+              );
+              break;
+            case 'landscape-secondary':
+              document.documentElement.style.setProperty(
+                '--heading',
+                `${heading + 180}`
+              );
+              break;
+            case 'portrait-primary':
+              document.documentElement.style.setProperty(
+                '--heading',
+                `${heading - 90}`
+              );
+              break;
+            case 'portrait-secondary':
+              document.documentElement.style.setProperty(
+                '--heading',
+                `${heading + 90}`
+              );
+          }
+        }
+      );
+      return sensor;
+    }
+    return undefined;
   }
 }
